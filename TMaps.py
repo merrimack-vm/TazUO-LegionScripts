995f7b0ca965e617e59b752a1a4ea3e0f2a71be0
merrimack-vm <31725951+merrimack-vm@users.noreply.github.com>
2026-09-07 17:59:03 +0200

    Add documentation for treasure map scanning script
    Added comments explaining the purpose of the script.

--- TMaps.py
+++ TMaps.py
@@ -1,6 +1,13 @@
 import API
 import re
 
+# ------------------------------------------------------------
+# INFO
+# ------------------------------------------------------------
+
+# Scans your backpack for the uncompleted treasure map closest to your location
+# Uses a shovel to dig on your location, if you are too far it shows you an arrow directing you to the chest location
+# Script intended to be used as a hotkey 
 
 # ------------------------------------------------------------
 # SETTINGS
